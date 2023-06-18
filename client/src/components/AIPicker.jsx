15a06e662770f427b2f420ac7a8e2f6a36dcd119
haksuman <h.aksuman@gmail.com>
2023-06-18 15:26:15 +0300

    first commit

---
+++ client/src/components/AIPicker.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+import CustomButton from "./CustomButton";
+
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask AI ..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="ai-picker-textarea"
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton type="outline" title="Asking AI..." customStyles="text-xs" />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI Logo"
+              handleClick={() => {
+                handleSubmit("logo");
+              }}
+              customStyles="text-xs"
+            />
+            <CustomButton
+              type="filled"
+              title="AI full"
+              handleClick={() => {
+                handleSubmit("full");
+              }}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AIPicker;
---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import state from "../store";
+import { useSnapshot } from "valtio";
+import { getContrastingColor } from "../config/helpers";
+
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  const snap = useSnapshot(state);
+
+  const generateStyles = (type) => {
+    if (type === "filled") {
+      return {
+        backgroundColor: snap.color,
+        color: getContrastingColor(snap.color),
+      };
+    } else if (type === "outline") {
+      return {
+        // backgroundColor: "transparent",
+        border: `1px solid ${snap.color}`,
+        color: snap.color,
+      };
+    }
+  };
+
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyles(type)}
+      onClick={handleClick}
+      title={title}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default CustomButton;
